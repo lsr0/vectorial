9fc83ef955f4329c8cbcf2a53468b9973ea06de6
Lindsay Roberts <m@lindsayr.com>
2015-09-04 08:27:45 +0000

    Optimise NEON matrix transpose.
    A zip based implementation of matrix transpose: 32% speed improvement on Cortex
    A15.

--- include/vectorial/simd4x4f_neon.h
+++ include/vectorial/simd4x4f_neon.h
@@ -6,23 +6,17 @@
 #ifndef VECTORIAL_SIMD4X4F_NEON_H
 #define VECTORIAL_SIMD4X4F_NEON_H
 
-
 vectorial_inline void simd4x4f_transpose_inplace(simd4x4f* s) {
-    const _simd4f_union sx = { s->x };
-    const _simd4f_union sy = { s->y };
-    const _simd4f_union sz = { s->z };
-    const _simd4f_union sw = { s->w };
-    
-    const simd4f dx = simd4f_create( sx.f[0], sy.f[0], sz.f[0], sw.f[0] );
-    const simd4f dy = simd4f_create( sx.f[1], sy.f[1], sz.f[1], sw.f[1] );
-    const simd4f dz = simd4f_create( sx.f[2], sy.f[2], sz.f[2], sw.f[2] );
-    const simd4f dw = simd4f_create( sx.f[3], sy.f[3], sz.f[3], sw.f[3] );
+    float32x4x2_t zip_13 = vzipq_f32(s->x, s->z);
+    float32x4x2_t zip_24 = vzipq_f32(s->y, s->w);
 
-    s->x = dx;
-    s->y = dy;
-    s->z = dz;
-    s->w = dw;
+    float32x4x2_t rows_xy = vzipq_f32(zip_13.val[0], zip_24.val[0]);
+    float32x4x2_t rows_zw = vzipq_f32(zip_13.val[1], zip_24.val[1]);
 
+    s->x = rows_xy.val[0];
+    s->y = rows_xy.val[1];
+    s->z = rows_zw.val[0];
+    s->w = rows_zw.val[1];
 }
 
 vectorial_inline void simd4x4f_transpose(const simd4x4f *s, simd4x4f *out) {
